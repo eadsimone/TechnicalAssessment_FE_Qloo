82774da33d75b4f1d1bfe7a9717027d5c2029118
Ezequiel <ezequiel@toptal.com>
2025-07-31 13:09:28 -0300

    add change name functionality

--- src/components/TimelineItem/TimelineItem.js
+++ src/components/TimelineItem/TimelineItem.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TimelineItem.css';
 
 const TimelineItem = ({ item, top, left, width }) => {
+    const [name, setName] = useState(item.name);
+
     return (
         <div
             className="timeline-item"
             style={{ top, left, width }}
-            title={item.name}
+            title={name}
         >
-            {item.name}
+            <div
+                contentEditable
+                suppressContentEditableWarning
+                onBlur={(e) => setName(e.target.innerText)}
+                className="timeline-item-name"
+            >
+                {name}
+            </div>
         </div>
     );
 };
